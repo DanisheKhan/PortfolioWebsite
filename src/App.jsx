62740ad1c8f8edfe8b4b82f67434a6ed16fb263c
Danish Khan <khandanishe125@gmail.com>
2025-05-25 15:32:55 +0530

    4. feat: add Hero and FlipWords components, update Navbar with name change, and enhance package dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './sections/Navbar.jsx'
+import Hero from './sections/Hero.jsx'
 
 export default function App() {
   return (
     <div className='container mx-auto max-w-7xl'>
       <Navbar />
+      <Hero />
     </div>
   )
 }
